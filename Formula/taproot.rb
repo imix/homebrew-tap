1793adf9bb519318c68e31e53195823bc21ecb15
taproot-ci <ci@imix.io>
2026-03-28 13:03:01 +0000

    taproot v0.8.6

--- Formula/taproot.rb
+++ Formula/taproot.rb
@@ -1,9 +1,9 @@
 class Taproot < Formula
   desc "AI-driven specs, enforced at commit time"
   homepage "https://github.com/imix/taproot"
-  url "https://github.com/imix/taproot/archive/refs/tags/v0.8.5.tar.gz"
-  sha256 "b4dc07ee478d15bcbfeebc470c9a95dcab6e5fdfb21da3bf35fbd9858fd61d4e"
-  version "0.8.5"
+  url "https://github.com/imix/taproot/archive/refs/tags/v0.8.6.tar.gz"
+  sha256 "6101887573144bc04848517474f9b2f84a2d32e80f7500c6fc979bbfe001a9fe"
+  version "0.8.6"
   license "MIT"
 
   depends_on "node"
